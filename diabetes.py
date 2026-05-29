9460fe56b4f252056279d26e9f2da9bc05616ce7
Shabarinathp <shabarinathpremlal@gmail.com>
2026-05-29 20:08:12 +0000

    Test automatic CI CD deployment final

--- diabetes.py
+++ diabetes.py
@@ -97,7 +97,7 @@ smoking_history_map = {
 
 
 # ---------------- APPLICATION UI ----------------
-st.title("🩺 Diabetes Prediction Ap shabarinathp")
+st.title("🩺 Diabetes Prediction Ap for public")
 st.write("Enter the patient details below to predict diabetes risk.")
 
 with st.form("prediction_form"):
